174bb104631fb2648237df67673a77c7463800e6
Williams <w.infanzon.98@gmail.com>
2025-02-25 14:56:24 +0100

    Creación de CRUD para el modelo libro

--- books/urls.py
+++ books/urls.py
@@ -1,16 +1,23 @@
 from django.urls import path
 
-from .views import editoriales_view, autores_view, autor_detail, libros_view, editorial_create, editorial_detail, EditorialList, EditorialDetail
+from .views import editoriales_view, autores_view, autor_detail, libros_view, editorial_create, editorial_detail, AutorList, AutorDetail, EditorialList, EditorialDetail, LibroCreateView, LibrosListView, LibroUpdateView, LibroDetail, LibroDeleteView
 
 app_name = "books"
 
 urlpatterns = [
     path("editoriales/", editoriales_view, name="editorial_list"),
-    path("editoriales/lista/", EditorialList.as_view(), name="editorial_list_ccbv"),
+    path("editoriales/list/", EditorialList.as_view(), name="editorial_list_ccbv"),
     path("editoriales/detalle/ccbv/<pk>/", EditorialDetail.as_view(), name="editorial_detail_ccbv"),
     path("editoriales/detalle/<int:id>/", editorial_detail, name="editorial_detail"),
     path("editoriales/create/", editorial_create, name="editorial_create"),
     path("autores/", autores_view, name="autor_list"),
     path("autores/<int:id>/", autor_detail, name="autor_detail"),
+    path("autores/list", AutorList.as_view(), name="autor_list_ccbv"),
+    path("autores/detail/<pk>", AutorDetail.as_view(), name="autor_detail_ccbv"),
     path("libros/", libros_view, name="libros_list"),
+    path("libros/list", LibrosListView.as_view(), name="libros_list"),
+    path("libros/detail/<pk>", LibroDetail.as_view(), name="libro_detail"),
+    path("libros/editar/<pk>/", LibroUpdateView.as_view(), name="libro_update"),
+    path("libros/nuevo", LibroCreateView.as_view(), name="libro_create"),
+    path("libros/eliminar/<pk>", LibroDeleteView.as_view(), name="libro_delete"),
 ]
--- books/views/libros_views.py
+++ books/views/libros_views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from books.models import Libro
+from django.urls import reverse_lazy
 
 def libros_view(request):
     libros = Libro.objects.all()
@@ -10,3 +13,41 @@ def libros_view(request):
         "titulo": "Lista de libros",
     }
     return render(request, 'libros/libros.html', context)
+
+class LibrosListView(ListView):
+    model = Libro
+    template_name = "libros/libros.html"
+    context_object_name = "libros"
+
+class LibroDetail(DetailView):
+    model = Libro
+    template_name = "libros/libro_detail.html"
+    context_object_name = "libro"
+
+class LibroCreateView(CreateView):
+    model = Libro
+    fields=[
+        "titulo",
+        "isbn",
+        "fecha_publicacion",
+        "numero_paginas"
+    ]
+    template_name = "libros/libro_create.html"
+    success_url = reverse_lazy('books:libros_list')
+
+class LibroUpdateView(UpdateView):
+    model = Libro
+    fields=[
+        "titulo",
+        "isbn",
+        "fecha_publicacion",
+        "numero_paginas"
+    ]
+    template_name = "libros/libro_update.html"
+    def get_success_url(self):
+        return reverse_lazy('books:libro_detail', kwargs={'pk': self.object.pk})
+    
+class LibroDeleteView(DeleteView):
+    model = Libro
+    template_name = "libros/libro_delete.html"
+    success_url = reverse_lazy('books:libros_list')
